bb3142e6f36347bfb4c1c6fd9758b6d3a916524a
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2022-04-25 17:20:15 +0200

    Update cpptask7.cpp

--- cpptask7.cpp
+++ cpptask7.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 //Problem1
 template<typename T>
-T getMaxRecur(const T* arr, size_t size) {
+T getMaxRecur(const T* arr, size_t size) 
+{
     if (size == 1)
         return arr[0];
     return max(arr[size - 1], getMaxRecur(arr, size - 1));
@@ -13,24 +14,33 @@ T getMaxRecur(const T* arr, size_t size) {
 
 //Problem2
 template<typename T>
-void pointerSort(const T* arr, const T** ptrs, size_t size) {
-    
-    
-}
-
-void swap(int* x, int* y)
+void pointerSort(const T* arr, const T** ptrs, size_t size) 
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    int flag = 1;
+
+    for (int i = size - 1; i > 0 && flag; i--)
+    {
+        flag = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (*ptrs[j] > *ptrs[j + 1])
+            {
+                arr = ptrs[j];
+                ptrs[j] = ptrs[j + 1];
+                ptrs[j + 1] = arr;
+                flag = 1;
+            }
+        }
+    }
 }
 
 template <typename T>
-void printArrPointer(const T** parr, size_t size) {
+void printArrPointer(const T** parr, size_t size)
+{
     int i;
     for (i = 0; i < size; i++)
-        cout << parr[i] << " ";
-    cout << endl; 
+        cout << *parr[i] << " ";
+    cout << endl;
 }
 
 int main() {
