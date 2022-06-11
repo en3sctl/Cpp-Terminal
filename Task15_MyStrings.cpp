f6304ffbd9e0685cfa48885962a8c13b9b06e325
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2022-06-11 18:17:35 +0200

    Update Task15_MyStrings.cpp

--- Task15_MyStrings.cpp
+++ Task15_MyStrings.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS = 1
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>   // strlen, strcpy, strcat, strcmp...
 #include <cctype>    // tolower, toupper
@@ -8,15 +8,19 @@ public:
     String(const char* s) {
         str = strcpy(new char[strlen(s) + 1], s);
     }
+    
     String(const String& s) {
         str = strcpy(new char[strlen(s.str) + 1], s.str);
     }
+    
     String() {
         str = nullptr;
     }
+    
     ~String() {
         delete[] str;
     }
+    
     String& operator= (const String& other) {
         if (this == &other) return *this;
         delete[] str;
@@ -27,27 +31,32 @@ public:
     friend std::ostream& operator << (std::ostream& stream, const String& a) {
         return stream << a.str;
     }
+    
     size_t length() {
         size_t length = 0;
         for (int i = 0; str[i] != '\0'; i++, length++);
         return length;
     }
+    
     String toUpper() {
         for (int i = 0; str[i] != '\0'; i++) {
             str[i] = toupper(str[i]);
         }
         return str;
     }
+    
     String toLower() {
         for (int i = 0; str[i] != '\0'; i++) {
             str[i] = tolower(str[i]);
         }
         return str;
     }
+    
     friend String operator+ (const String& a, const String& b);
     friend String operator+ (const char* a, const String& b);
     friend String operator+ (const String& a, const char* b);
 };
+
 // ...
 String operator+ (const String& a, const String& b) {
     char* cstr = new char[strlen(a.str) + strlen(b.str) + 1];
@@ -57,6 +66,7 @@ String operator+ (const String& a, const String& b) {
     x.str = cstr;
     return x;
 }
+
 String operator+ (const char* a, const String& b) {
     char* cstr = new char[strlen(a) + strlen(b.str) + 1];
     strcpy(cstr, a);
@@ -65,6 +75,7 @@ String operator+ (const char* a, const String& b) {
     x.str = cstr;
     return x;
 }
+
 String operator+ (const String& a, const char* b) {
     char* cstr = new char[strlen(a.str) + strlen(b) + 1];
     strcpy(cstr, a.str);
@@ -73,13 +84,8 @@ String operator+ (const String& a, const char* b) {
     x.str = cstr;
     return x;
 }
+
 int main() {
-    char b[] = "Hello";
-    String mystring(b);
-    std::cout << mystring << std::endl;
-    String anotherStr(mystring);
-    std::cout << anotherStr << std::endl;
-    std::cout << anotherStr + mystring << std::endl;
     String s = ("To " + String("be ") +
         "or not to be").toUpper() + "!";
     std::cout << s << std::endl;
